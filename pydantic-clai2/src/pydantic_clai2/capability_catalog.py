407fdbe0376cfaf7ba18a4286ea3fca17330804a
Ojus Save <ojusave@users.noreply.github.com>
2026-09-24 08:37:01 -0700

    fixed readme

--- pydantic-clai2/src/pydantic_clai2/capability_catalog.py
+++ pydantic-clai2/src/pydantic_clai2/capability_catalog.py
@@ -36,7 +36,6 @@ _FACTORIES = (
     ('playwright', 'playwright:PlaywrightBrowser'),
     ('prompt_injection_defender', 'prompt_injection_defender:PromptInjectionDefender'),
     ('pydantic_ai_docs', 'pydantic_ai_docs:PydanticAIDocs'),
-    ('render_workflows', 'render:RenderWorkflows'),
     ('repair_tool_arguments', 'repair_tool_arguments:RepairToolArguments'),
     ('researcher', 'researcher:Researcher'),
     ('shell', 'shell:Shell'),
